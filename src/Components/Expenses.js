e39564249f46f040607062dec4275cc14ba5922f
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-19 09:58:48 +0600

    ln:47,  Initial project structure

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -16,8 +16,7 @@ function Expenses() {
 
     return (
         <div className="container text-center" >
-            <h1>Lets started... centered branch -development </h1>
-            <h1>Lets started... centered branch -development new </h1>
+
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-10 col-md-10">
                     <MyCard className="expenses">
